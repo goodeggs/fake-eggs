45605d23a3597418b28944de60020990df24dc65
dannynelson <danny@goodeggs.com>
2017-05-03 16:40:48 -0700

    Fix bug in day calculation

--- src/day/index.js
+++ src/day/index.js
@@ -7,7 +7,7 @@ import date from '../date';
 export default function day (from?: Date | string, to?: Date | string): string {
   const _date = date(from, to);
   const year = _.padStart(String(_date.getFullYear()), 4, '0');
-  const month = _.padStart(String(_date.getMonth()) + 1, 2, '0');
+  const month = _.padStart(String(_date.getMonth() + 1), 2, '0');
   const dayOfMonth = _.padStart(String(_date.getDate()), 2, '0');
   return `${year}-${month}-${dayOfMonth}`;
 }
