7d4bb1d6b9463cb0b27c2a18d02899de635644c2
Danny Nelson <danny@goodeggs.com>
2018-01-11 11:16:05 -0800

    Improve test coverage

--- src/test.js
+++ src/test.js
@@ -1,50 +1,46 @@
 // @flow
 import {describe, it} from 'mocha';
+import {expect} from 'goodeggs-test-helpers/chai';
 
 import fake from '../src'
 
 describe('the default export', function() {
+  // TODO: split these into smaller tests
   it('works', function () {
-    fake.customer.firstName();
-    fake.customer.lastName();
-    fake.customer.phoneNumber();
-    fake.customer.email();
-    
-    fake.employee.firstName();
-    fake.employee.lastName();
-    fake.employee.phoneNumber();
-    fake.employee.email();
-    
-    fake.producer.name();
-    fake.producer.slug();
-    
-    fake.product.name();
-    fake.product.unit();
-    fake.product.count();
-    fake.product.storageType();
-    
-    fake.foodhub.slug();
-    
-    fake.warehouseLocation.zone();
-    fake.warehouseLocation.aisle();
-    fake.warehouseLocation.rack();
-    fake.warehouseLocation.shelf();
-    fake.warehouseLocation.label();
-    
-    fake.inventoryLot.label();
-    
+    expect(fake.customer.firstName()).to.be.a('string');
+    expect(fake.customer.lastName()).to.be.a('string');
+    expect(fake.customer.phoneNumber()).to.be.a('string');
+    expect(fake.customer.email()).to.be.a('string');
+    expect(fake.employee.firstName()).to.be.a('string');
+    expect(fake.employee.lastName()).to.be.a('string');
+    expect(fake.employee.phoneNumber()).to.be.a('string');
+    expect(fake.employee.email()).to.be.a('string');
+    expect(fake.producer.name()).to.be.a('string');
+    expect(fake.producer.slug()).to.be.a('string');
+    expect(fake.product.name()).to.be.a('string');
+    expect(fake.product.unit()).to.be.a('string');
+    expect(fake.product.count()).to.be.a('number');
+    expect(fake.product.storageType()).to.be.a('string');
+    expect(fake.foodhub.slug()).to.be.a('string');
+    expect(fake.warehouseLocation.zone()).to.be.a('string');
+    expect(fake.warehouseLocation.aisle()).to.be.a('string');
+    expect(fake.warehouseLocation.rack()).to.be.a('string');
+    expect(fake.warehouseLocation.shelf()).to.be.a('string');
+    expect(fake.warehouseLocation.label()).to.be.a('string');
+    expect(fake.inventoryLot.label()).to.be.a('string');
+    // TODO: better test for this
     fake.maybe(fake.boolean);
-    fake.array(0, 2, fake.boolean);
-    fake.boolean();
-    fake.number();
-    fake.date();
-    fake.day();
-    fake.tzid();
-    fake.objectId();
-    fake.integer();
-    fake.sample(['a','b','c']);
-    fake.digit();
-    fake.string();
-    fake.uri();
+    expect(fake.array(0, 2, fake.boolean)).to.be.an.instanceof(Array);
+    expect(fake.boolean()).to.be.a('boolean');
+    expect(fake.number()).to.be.a('number');
+    expect(fake.date()).to.be.an.instanceof(Date);
+    expect(fake.day()).to.be.a('string');
+    expect(fake.tzid()).to.be.a('string');
+    expect(fake.objectId()).to.be.a('string');
+    expect(fake.integer()).to.be.a('number');
+    expect(fake.sample(['a','b','c'])).to.be.a('string');
+    expect(fake.digit()).to.be.a('number');
+    expect(fake.string()).to.be.a('string');
+    expect(fake.uri()).to.be.a('string');
   });
 });
