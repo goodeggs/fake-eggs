c5d5bdc32d7ca65c0ceea83a0cfc56f13024c911
Nathan Houle <nathan@nathanhoule.com>
2018-06-12 13:11:58 -0700

    Update maybe, optional docs with Flow usage example

--- src/maybe/index.js
+++ src/maybe/index.js
@@ -6,14 +6,18 @@ const returnNull = () => null;
 
 /**
  * Potentially returns `null`, `undefined`, or the result of the supplied `returnValue` function.
+ *
+ * Useful for maybe types in Flow, e.g.:
+ *
+ * ```
+ * {
+ *   maybeValue: ?boolean,
+ * }
+ * ```
  */
-function maybe <T> (returnValue: () => T): ?T {
-  const getter = sample([
-    returnUndefined,
-    returnNull,
-    returnValue,
-  ]);
-  
+function maybe<T>(returnValue: () => T): ?T {
+  const getter = sample([returnUndefined, returnNull, returnValue]);
+
   return getter();
 }
 
--- src/optional/index.js
+++ src/optional/index.js
@@ -5,6 +5,14 @@ const returnUndefined = () => undefined;
 
 /**
  * Returns `undefined` or the result of the supplied `returnValue` function.
+ *
+ * Useful for specifying optional object properties in Flow, e.g.:
+ *
+ * ```
+ * {
+ *   optionalValue?: boolean,
+ * }
+ * ```
  */
 export default function optional<T>(returnValue: () => T): T | void {
   const getter = sample([returnUndefined, returnValue]);
