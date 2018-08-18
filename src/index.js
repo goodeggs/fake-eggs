e5d0065b9ab9d1c41f9f39959b0e42333d414d1b
Max Edmands <max@goodeggs.com>
2018-08-18 08:09:06 -0700

    Add globallyUniqueString function

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
 // @flow
-import array from './array';
-import boolean from './boolean';
-import customer from './customer';
-import date from './date';
-import day from './day';
-import digit from './digit';
-import email from './email';
-import employee from './employee';
-import factory from './factory';
-import firstName from './first_name';
-import foodhub from './foodhub';
-import fullName from './full_name';
-import integer from './integer';
-import inventoryLot from './inventory_lot';
-import lastName from './last_name';
-import maybe from './maybe';
-import number from './number';
-import objectId from './object_id';
-import optional from './optional';
-import phoneNumber from './phone_number';
-import producer from './producer';
-import product from './product';
-import sample from './sample';
-import string from './string';
-import tzid from './tzid';
-import uri from './uri';
-import warehouseLocation from './warehouse_location';
+import array from "./array";
+import boolean from "./boolean";
+import customer from "./customer";
+import date from "./date";
+import day from "./day";
+import digit from "./digit";
+import email from "./email";
+import employee from "./employee";
+import factory from "./factory";
+import firstName from "./first_name";
+import foodhub from "./foodhub";
+import fullName from "./full_name";
+import globallyUniqueString from "./globally_unique_string";
+import integer from "./integer";
+import inventoryLot from "./inventory_lot";
+import lastName from "./last_name";
+import maybe from "./maybe";
+import number from "./number";
+import objectId from "./object_id";
+import optional from "./optional";
+import phoneNumber from "./phone_number";
+import producer from "./producer";
+import product from "./product";
+import sample from "./sample";
+import string from "./string";
+import tzid from "./tzid";
+import uri from "./uri";
+import warehouseLocation from "./warehouse_location";
 
 export default {
   array,
@@ -40,6 +41,7 @@ export default {
   firstName,
   foodhub,
   fullName,
+  globallyUniqueString,
   integer,
   inventoryLot,
   lastName,
