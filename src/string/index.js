887c408a205b399491d8cacc347b7cd60ac60a05
Davidson <davidson.nascimento@goodeggs.com>
2020-01-08 17:36:20 -0300

    improvement: remove fake-string double space

--- src/string/index.js
+++ src/string/index.js
@@ -1,18 +1,30 @@
 // @flow
 
-import _ from 'lodash';
+import _ from "lodash";
 
-import sample from '../sample';
-import integer from '../integer';
+import sample from "../sample";
+import integer from "../integer";
+
+export const removeDoubleSpace = (str: string): string => str.replace(/\s+/g, " ");
 
 /**
  * Generates a random string, optionally of `length` and using chars from provided `charset`.
  */
-function string (length?: number, charset?: string): string {
+function string(length?: number, charset?: string): string {
   if (length == null) length = integer(1, 40);
-  if (charset == null) charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ';
-  const chars = charset.split('');
-  return _.times(length, () => sample(chars)).join('');
+  if (charset == null)
+    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+  const chars = charset.split("");
+
+  let str = "";
+
+  for (; str.length < length; ) {
+    str += sample(chars);
+
+    if (str.length === length) str = removeDoubleSpace(str).trim();
+  }
+
+  return str;
 }
 
 export default string;
---
+++ src/string/test.js
@@ -0,0 +1,42 @@
+// @flow
+import _ from "lodash";
+import { describe, it } from "mocha";
+import { expect } from "goodeggs-test-helpers";
+
+import integer from "../integer";
+import string, { removeDoubleSpace } from ".";
+
+const removeDuplicates = (str: string): string[] => _.uniq(str.split(""));
+
+describe("string", function() {
+  it("generates a random string", function() {
+    expect(string()).to.be.an("string");
+  });
+
+  it("generates a random string with legth passed-in", function() {
+    const length = integer(1);
+
+    expect(string(length).length).to.be.equal(length);
+  });
+
+  it("generates a random string with charset passed-in", function() {
+    const charset = Math.random().toString(36); // generate a random charset
+    const length = integer(100);
+    const chars = removeDuplicates(string(length, charset));
+    const charsetChars = removeDuplicates(charset);
+
+    expect(chars)
+      .to.be.an("array")
+      .that.has.length(charsetChars.length)
+      .that.have.members(charsetChars);
+  });
+
+  it("generates a random without double space and right side space", function() {
+    const charset = "a      bc ";
+    const generatedString = string(integer(100), charset);
+
+    expect(generatedString.length).to.be.equal(
+      removeDoubleSpace(generatedString).length
+    );
+  });
+});
