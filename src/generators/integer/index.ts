5dfb77cdcd47f4e8f65fc14ad254bb6a4e7de820
Shermam Miranda <shermam.miranda@AC-LPT-00026.local>
2020-10-07 14:45:34 -0300

    Set better defaults for `fake.integer` and `fake.date`
    Fake.integer a default min set to -100 and a default max set to 100.
    This is problematic because it:
    1. Occludes `chance.js` default range of -9007199254740991 to 9007199254740991
    2. If only the lower param is provided and it is greater than 100 an error will be thrown
    
    Fake.date also had similar problems with `from` and `to`. Now I am setting
    `from` to be any valid date. And `to` to be a date that is always after `from`.

--- src/generators/integer/index.ts
+++ src/generators/integer/index.ts
@@ -4,13 +4,13 @@ import {Chance} from 'chance';
  * Generates a random integer (could be negative!). Optionally between `lowerExclusive` and `upperExclusive`.
  */
 const createIntegerGenerator = (chance: Chance.Chance) => (
-  lowerInclusive = -100,
-  upperExclusive = 100,
+  lowerInclusive?: number,
+  upperExclusive?: number,
 ): number =>
   chance.integer({
     min: lowerInclusive,
     // chance.integer's max param is inclusive
-    max: upperExclusive - 1,
+    max: upperExclusive && upperExclusive - 1,
   });
 
 export default createIntegerGenerator;
