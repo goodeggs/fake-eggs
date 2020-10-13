061f6101fd4ec1c85eb8e78030a20d5f2510c2f1
Shermam Miranda <shermam.miranda@AC-LPT-00026.local>
2020-10-13 12:38:19 -0300

    Update docs for `fake.integer`

--- src/generators/integer/index.ts
+++ src/generators/integer/index.ts
@@ -2,6 +2,8 @@ import {Chance} from 'chance';
 
 /**
  * Generates a random integer (could be negative!). Optionally between `lowerExclusive` and `upperExclusive`.
+ * If `lowerInclusive` or `upperExclusive` are not supplied Chance.js will use the range: -9007199254740991 to 9007199254740991
+ * https://chancejs.com/basics/integer.html
  */
 const createIntegerGenerator = (chance: Chance.Chance) => (
   lowerInclusive?: number,
