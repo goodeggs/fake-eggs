14b431ae025e5aabd49276c5e645ae547e060acb
Rishabh Sharma <rishabh.sharma@goodeggs.com>
2021-02-08 14:31:22 -0800

    chore: improved upperExclusive validation

--- src/generators/number/index.ts
+++ src/generators/number/index.ts
@@ -1,4 +1,4 @@
-import {Chance} from 'chance';
+import { Chance } from 'chance';
 
 /**
  * Generates a random `number`, optionally between `lowerInclusive` and `upperExclusive` with `fixed` number of digits after the decimal.
@@ -7,11 +7,19 @@ const createNumberGenerator = (chance: Chance.Chance) => (
   lowerInclusive = -1000000,
   upperExclusive = 1000000,
   fixed = 4,
-): number =>
-  chance.floating({
+): number => {
+  const EPSILON = 0.0000001;
+  const MAX_PRECISION = 6;
+
+  if (upperExclusive != null && upperExclusive > MAX_PRECISION) {
+    throw new TypeError(`"upperExclusive" must be less than or equal to ${MAX_PRECISION} or null`);
+  }
+
+  return chance.floating({
     min: lowerInclusive,
-    max: upperExclusive,
+    max: upperExclusive - EPSILON,
     fixed,
   });
+}
 
 export default createNumberGenerator;
