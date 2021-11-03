0ca401de5ff7fc7a57566d9935c33bcc9b67df92
Vivian <vvnsze@gmail.com>
2021-11-03 12:41:30 -0700

    chore(foodhub):add VRN1

--- src/generators/foodhub/index.ts
+++ src/generators/foodhub/index.ts
@@ -3,7 +3,7 @@ import {Chance} from 'chance';
 import createSampleGenerator from '../sample';
 
 // TODO(ndhoule): Use goodeggs-foodhubs
-const foodhubSlugs = ['sfbay', 'la', 'nola', 'nyc', 'MAR1', 'OKN2'];
+const foodhubSlugs = ['sfbay', 'la', 'nola', 'nyc', 'MAR1', 'OKN2', 'VRN1'];
 
 /**
  * Returns a randomly-selected foodhub slug, e.g. `sfbay`.
